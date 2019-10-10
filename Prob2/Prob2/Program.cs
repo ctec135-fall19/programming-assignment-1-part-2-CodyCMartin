4fe4646b0cb9a495cd6fe13c63bfe6922976e066
codycmartin <codycmartin89@gmail.com>
2019-10-10 09:12:51 -0700

    Prob 2 Complete. Prob 3 WIP

---
+++ Prob2/Prob2/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+Author:  Cody Martin
+Date: 10/9/19
+CTEC 135: Microsoft Software Development with C#
+
+<Module 2, Programming Assignment 1 part 2 , Problem 2 >
+
+   <Iteration Constructs>
+
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+       
+        {
+            #region print 1-5 with spaces between the numbers on a single line using a for loop
+            for (int i=0; i<6; i++)
+            {
+                Console.Write(i);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+
+            #endregion
+
+            #region print 1-5 with spaces between the numbers on a single line using a while loop
+            int count = 0;
+            while (count < 6)
+                
+            {
+                Console.Write(count);
+                count++;
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+            #endregion
+
+            #region print 1-5 with spaces between the numbers on a single line using a do/while loop
+            int doCount = 0;
+            do
+            {
+                Console.Write(doCount);
+                doCount++;
+                Console.Write(" ");
+
+
+            } 
+            while (doCount < 6);
+            Console.WriteLine();
+            #endregion
+
+           
+
+        }
+    }
+}
---
+++ prob3/prob3/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Author:  Cody Martin
+Date: 10/9/19
+CTEC 135: Microsoft Software Development with C#
+
+<Module 2, Programming Assignment 1 part 2 , Problem 3 >
+
+   <Decision Constructs>
+
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prob3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

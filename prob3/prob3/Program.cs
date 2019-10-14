57d3c5efb47aa7cdd045dc2300a47e84fc987219
codycmartin <codycmartin89@gmail.com>
2019-10-14 00:10:37 -0700

    Region 1 prob 3 complete

--- prob3/prob3/Program.cs
+++ prob3/prob3/Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+Author:  Cody Martin
+Date: 10/9/19
+CTEC 135: Microsoft Software Development with C#
+
+<Module 2, Programming Assignment 1 part 2 , Problem 3 >
+
+   <Printer problem>
+
+*/using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,16 +21,81 @@ namespace prob3redo
         { // for loops for conditions
             for (int printerPrints = 0; printerPrints < 8; printerPrints++)
             {
-                for (int lightFlashing = 0; lightFlashing < 8; printerPrints++)
+                for (int lightFlashing = 0; lightFlashing < 8; lightFlashing++)
                 {
                     for (int comp = 0; comp < 8; comp++)
                     {
-                        // if states to check conditions
-                        if (printerPrints == 1 && lightFlashing == 1 && comp == 1)
+                        // if states to check conditions and print troubleshooting 
+                        if (printerPrints == 0 && lightFlashing == 0 && comp == 0)
                         {
+                            Console.WriteLine("When 1.The printer isnt printing 2.Red light is flashing 3.Printer isnt recognized by computer");
                             PrinterCable();
                             PrinterSoftware();
                             CheckInk();
+                            Console.WriteLine();
+                            
+                            
+                            
+                        }
+                        if (printerPrints == 1 && lightFlashing == 1 && comp == 1)
+                        {
+                            Console.WriteLine("When 1.The printer isnt printing 2.Red light is flashing 3.Printer i recognized by computer");
+                            
+                            PrinterJam();
+                            CheckInk();
+                            Console.WriteLine();
+                        }
+                        if (printerPrints == 2 && lightFlashing == 2 && comp == 2)
+                        {
+                            Console.WriteLine("When 1.The printer isnt printing 2.Red light is flashing 3.Printer isnt recognized by computer");
+                            PrinterCable();
+                            CheckInk();
+                            Console.WriteLine();
+                         
+                                                   
+                        }
+                        if (printerPrints == 3 && lightFlashing == 3 && comp == 3)
+                        {
+                            Console.WriteLine("When 1.The printer isnt printing 2.Red light is not flashing 3.Printer isnt recognized by computer");
+                            PrinterCable();
+                            PrinterSoftware();
+                            PrinterPower();
+                            Console.WriteLine();
+                        }
+                        if (printerPrints == 4 && lightFlashing == 4 && comp == 4)
+                        {
+                            Console.WriteLine("When 1.The printer isnt printing 2.Red light is not flashing 3.Printer is recognized by computer");
+                            PrinterJam();
+                            Console.WriteLine();
+                            
+                            
+                            
+                        }
+                        if (printerPrints == 5 && lightFlashing == 5 && comp == 5)
+                        {
+                            Console.WriteLine("When 1.The printer is printing 2.Red light is not flashing 3.Printer isnt recognized by computer");
+                            
+                            PrinterSoftware();
+                            
+                            Console.WriteLine();
+                        }
+                        if (printerPrints == 6 && lightFlashing == 6 && comp == 6)
+                        {
+                            Console.WriteLine("When 1.The printer is printing 2.Red light is flashing 3.Printer is recognized by computer");
+                            
+                            CheckInk();
+                            Console.WriteLine();
+                            
+                            
+                            
+                        }
+                        if (printerPrints == 7 && lightFlashing == 7 && comp == 7)
+                        {
+                            Console.WriteLine("When 1.The printer is printing 2.Red light is not flashing 3.Printer isnt recognized by computer");
+                            
+                            PrinterSoftware();
+                            
+                            Console.WriteLine();
                         }
                     }
                 }
@@ -30,5 +104,28 @@ namespace prob3redo
             
         
         }
+
+        // action methods
+        static void PrinterCable()
+        {
+            Console.WriteLine("Check Printer To Computer Cable");
+        }
+        static void PrinterSoftware()
+        {
+            Console.WriteLine("Check Software");
+        }
+        static void CheckInk()
+        {
+            Console.WriteLine("Check Ink");
+        }
+        static void PrinterPower()
+        {
+            Console.WriteLine("Check Power Cable");
+        }
+        static void PrinterJam()
+        {
+            Console.WriteLine("Check for Jam");
+        }
+
     }
 }
